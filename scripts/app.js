367e560ed43be895296163dd845f99bdb728769e
Carson Butterfield <carsonbutterfield98@gmail.com>
2021-06-12 22:09:50 -0700

    connecting front to back end

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,4 @@
+const API = 'http://localhost:3000'
 let masterNode
 
 const renderFactories = () => {
@@ -8,11 +9,37 @@ const renderFactories = () => {
 }
 axios.get('http://localhost:3000').then(res => {
     masterNode = res.data
-    console.log(masterNode)
+    renderFactories()
 }).catch(err => {
     console.log(err)
 })
 
 $('#root').click(e => {
     renderFactories()
+})
+
+$('#factories').on('click', '.delete-factory', function(e){
+    axios.delete(`${API}/factories/${this.parentElement.id}`)
+    .then(res => {
+        masterNode = res.data;
+        renderFactories()
+    }).catch(err => console.log(err))
+
+})
+
+$('#factories').on('click', '.create-children', function(e){
+    axios.put(`${API}/factories/${this.parentElement.id}/children`)
+    .then(res => {
+        console.log(res.data)
+        masterNode = res.data;
+        renderFactories()
+    }).catch(err => console.log(err))
+})
+
+$('#create-factory').click(e=> {
+    axios.post(`${API}/factories`)
+    .then(res => {
+        masterNode = res.data
+        renderFactories()
+    })
 })
--- scripts/templates/factory.js
+++ scripts/templates/factory.js
@@ -4,6 +4,8 @@
 const factoryTemplate = (factory) => `
     <div id=${factory._id} class="factory">
         <h2> ${factory.name} </h2>
+        <button class="delete-factory">Delete </button>
+        <button class="create-children">Generate Numbers </button>
         <h4> Child Count: ${factory.childCount} <h4>
         <ul class="factory-children"> </ul>
 `;
@@ -14,7 +16,7 @@ const childTemplate = (child) => {
 
 const createFactory = ( factory ) => {
     $('#factories').append(factoryTemplate(factory))
-    const newEl = $(`#${factory._id}`)
+    const newEl = $(`#${factory._id} .factory-children`)
     factory.children.forEach(num => {
         newEl.append(childTemplate(num))
     })
